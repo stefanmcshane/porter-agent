c534da7c57b03cba9f7047ecc6a448ce39bdfb34
meehawk <meehawk@yahoo.com>
2022-09-28 15:22:49 -0400

    chore: make permission to 0600

--- pkg/logstore/memorystore/memorystore.go
+++ pkg/logstore/memorystore/memorystore.go
@@ -50,7 +50,7 @@ func New(name string, options Options) (*MemoryStore, error) {
 	logFileDir := options.Dir
 
 	if logFileDir == "" {
-		logFileDir = "/var/tmp"
+		logFileDir = filepath.Join("var", "tmp")
 	}
 
 	store.location = path.Join(logFileDir, name+".log")
@@ -96,7 +96,7 @@ func (store *MemoryStore) Stop() error {
 func (store *MemoryStore) Push(log string) error {
 	logFilePath := store.location
 
-	f, err := os.OpenFile(logFilePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	f, err := os.OpenFile(logFilePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0600)
 
 	if err != nil {
 		return fmt.Errorf("error opening log file for memory store with name %s. Error: %w", store.name, err)
